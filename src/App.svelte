b9039afc3a77ff8ac3c9686643bc5be3bb881976
sveltemaster <sveltemaster@gmail.com>
2020-03-07 12:17:17 -0800

    part 1

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+	import { FirebaseApp, User, Doc, Collection } from "sveltefire";
+	import firebase from "firebase/app";
+	import "firebase/firestore";
+	import "firebase/auth";
+
+	const firebaseConfig = {
+		apiKey: "AIzaSyD-kkgrWJ3sh53XWyxwTI3yu000pZCUVpI",
+		authDomain: "storagepractice-b2dcb.firebaseapp.com",
+		databaseURL: "https://storagepractice-b2dcb.firebaseio.com",
+		projectId: "storagepractice-b2dcb",
+		storageBucket: "storagepractice-b2dcb.appspot.com",
+		messagingSenderId: "605741809709",
+		appId: "1:605741809709:web:480fec11af2a01611c38a4"
+	};
+  	// Initialize Firebase
+  	firebase.initializeApp(firebaseConfig);
+</script>
